c58a35d5d5f6dcf122dbdb9f90f71e21ca7afae4
Emmanuel Okeibunor <okeibunoremma@gmail.com>
2022-04-04 11:00:57 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="main-nav">
+    <div class="logo">
+      <img src="./assets/images/Logo.svg" alt="" srcset="" />
+    </div>
+    <div class="nav-bar">
+      <div class="nav-links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">Market</router-link>
+        <router-link to="/about">News</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <div class="search-form">
+        <form action="#" method="post">
+          <input
+            type="text"
+            name="searchInput"
+            id="searchInput"
+            class="search"
+            placeholder="Search tokens..."
+          />
+          <button type="submit">
+            <img src="./assets/images/search_icon.svg" alt="" srcset="" />
+          </button>
+        </form>
+      </div>
+      <div class="avater">
+        <div class="login_btn"></div>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Source Sans Pro", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -16,15 +42,53 @@
 }
 
 nav {
-  padding: 30px;
-
+  display: grid;
+  width: 100%;
+  grid-template-columns: 20% auto;
+  .logo {
+    justify-content: center;
+    align-self: center;
+  }
+  .nav-bar {
+    grid-column: 2;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-self: center;
+    margin: auto 2.75rem;
+  }
+  .nav-links {
+    margin: auto 1.5rem;
+  }
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    margin: auto 1.5rem;
+    font-weight: 600;
+    font-size: 16px;
+    color: #0d1b1e;
+    text-decoration: none;
+    &.router-link-active {
+      color: #006fe8;
+      border-bottom: 2px #006fe8 solid;
+      padding-bottom: 2px;
+      border-radius: 25px;
     }
+  }
+  #searchInput {
+    margin: auto;
+    background: rgba(173, 185, 227, 0.7);
+    border-radius: 10px;
+    border: none;
+    padding: 12px 16px;
+    width: 240px;
+    height: 16px;
+    font-weight: 400;
+  }
+  button {
+    position: relative;
+    background: rgba(255, 255, 255, 0);
+    border: none;
+    top: 4px;
+    right: 36px;
   }
 }
 </style>
