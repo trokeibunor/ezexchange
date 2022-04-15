586a69a4bfb668f44ecf19054d207dfb05c10222
Emmanuel Okeibunor <okeibunoremma@gmail.com>
2022-04-15 23:04:30 +0100

    finished about page design

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,1 +1,396 @@
+<template>
+  <div class="container-fluid" id="about-hero">
+    <nav-component />
+    <div class="about-hero">
+      <img src="../assets/images/about-hero.png" alt="" srcset="" />
+      <div class="text-holder">
+        <h3 class="header">About us</h3>
+        <h2>What's Our deal what's our main goal?</h2>
+        <p class="desc">
+          We at Ez-exchange saw the problem with peer-to-peer exchanges in
+          countries where there is a crackdown on crypto currencies. Sed nulla
+          odio, imperdiet vel metus quis, vehicula sodales eros. Donec mollis
+          faucibus lorem, rhoncus volutpat augue bibendum vel. Vivamus semper
+          vel purus eget vulputate. Quisque sodales sapien nec justo ultrices
+          rutrum. Pellentesque et quam quis purus tristique interdum. Fusce sit
+          amet nunc scelerisque, ultricies mauris at, porta libero. Vivamus
+          mollis lorem ut nulla porttitor, et rutrum arcu eleifend. Suspendisse
+          id neque rutrum, euismod diam in, molestie tellus. Phasellus sed leo
+          ut dolor eleifend rutrum. Aliquam ac luctus nibh. Suspendisse posuere,
+          massa eu rhoncus malesuada, augue dolor facilisis nisi, venenatis
+          fringilla erat est id diam. Donec vel enim commodo, posuere enim eget,
+          sollicitudin leo. Nam placerat lorem nec pulvinar rhoncus. Morbi quis
+          venenatis enim. Mauris aliquet lorem urna, vel venenatis metus
+          tristique nec.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid" id="operation">
+    <div class="operation">
+      <div class="header">
+        <h3>How we Operate</h3>
+        <div></div>
+      </div>
+      <div class="content">
+        <div class="text-content">
+          <h4>What't our modus operandi</h4>
+          <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            consectetur accumsan mollis. Pellentesque habitant morbi tristique
+            senectus et netus et malesuada fames ac turpis egestas. Pellentesque
+            euismod bibendum consectetur. Curabitur pellentesque neque id sem
+            finibus auctor. Aenean non tortor lectus. Suspendisse ultricies sit
+            amet turpis sit amet semper. Aliquam vel convallis orci.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Integer non magna ultricies massa facilisis
+            convallis. Vivamus fermentum varius tempus. Vestibulum ante ipsum
+            primis in faucibus orci luctus et ultrices posuere cubilia curae;
+            Donec quis viverra dolor, vitae mattis lacus. Phasellus vel tempus
+            ex.
+          </p>
+        </div>
+        <img src="https://picsum.photos/200" alt="" srcset="" />
+      </div>
+    </div>
+    <div class="beginnings">
+      <img src="https://picsum.photos/200" alt="" srcset="" />
+      <div class="text-content">
+        <h3 class="title">Starting Point</h3>
+        <h4 class="sub-title">How did we start</h4>
+        <p id="desc">
+          In hac habitasse platea dictumst. Integer et mi vel ex placerat
+          tristique eget in justo. Nam molestie, massa non scelerisque
+          convallis, nibh purus cursus leo, in placerat libero orci sed odio.
+          Praesent eu viverra risus. Nulla tellus enim, efficitur eu ultricies
+          at, tristique at nibh. Nulla in massa eros. Proin dolor purus,
+          elementum id sodales a, scelerisque a urna. Vivamus cursus sodales
+          nisi, sit amet fermentum nisl semper quis. Sed eleifend blandit
+          ultricies. Morbi at aliquam elit. Ut egestas at nisi non vestibulum.
+          Sed lacinia urna nec enim tempor, eget tincidunt augue sollicitudin.
+          Cras vel nisi arcu. Aenean elementum justo et odio egestas iaculis.
+          Nullam dictum leo a magna convallis interdum. Fusce hendrerit
+          tincidunt lorem, a consequat velit dapibus id. Pellentesque non mi
+          sodales, faucibus magna rutrum, fermentum risus. Vivamus nec sem est.
+          Sed pulvinar eget quam nec mollis. Donec varius scelerisque erat, eget
+          fringilla mauris eleifend vitae. Fusce quis pharetra lorem. Nullam
+          diam nibh, sodales non porttitor eu, tincidunt eu neque
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid" id="about-founder">
+    <div class="about-founder">
+      <h3 class="title">About Our founder</h3>
+      <div class="content">
+        <img src="../assets/images/femi_image.png" alt="" srcset="" />
+        <div class="text-content">
+          <p class="big">Meet Ezo</p>
+          <p id="femi-about">
+            Vestibulum non elementum lacus. Nullam enim dui, molestie efficitur
+            rutrum eu, facilisis sit amet leo. Donec tristique arcu accumsan sem
+            bibendum sagittis. Aliquam erat volutpat. Pellentesque ante quam,
+            venenatis ac nulla a, semper aliquet massa. Mauris ultrices purus
+            vel ipsum posuere scelerisque. Aliquam eget nisl eget velit
+            sollicitudin mollis. Quisque nisl purus, cursus ac est non,
+            consequat euismod mauris. Duis vel lobortis odio. Donec rutrum quis
+            leo non tristique. In velit magna, accumsan at luctus eget,
+            venenatis rutrum ligula. Praesent ac maximus ipsum. Cras et enim sit
+            amet ipsum mollis consequat eget vel diam. Donec nec felis eget
+          </p>
+          <p id="femi-about">
+            turpis rutrum dignissim sed sit amet metus. Aenean fermentum felis
+            et blandit convallis. Suspendisse sollicitudin diam eu semper
+            pellentesque. Aliquam augue orci, consequat efficitur libero ut,
+            blandit semper diam. Ut eu iaculis sem, vel imperdiet metus.
+            Suspendisse felis leo, euismod ac leo sed, fringilla dapibus tortor.
+            Curabitur placerat odio mauris, at sodales tortor cursus eget.
+            Integer vel rhoncus sapien. Maecenas consequat ut massa nec
+            pulvinar. Nunc bibendum diam vitae leo sodales auctor. Maecenas
+            pulvinar aliquam diam eu commodo. Nullam odio nibh, convallis a
+            risus in, commodo egestas risus. Phasellus rutrum urna a turpis
+            gravida, in feugiat felis convallis. Etiam lacinia nisl at leo
+            tempor gravida. Aliquam risus ex, posuere id odio ac, volutpat
+            consectetur diam.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid" id="contact-us">
+    <div class="contact">
+      <div class="desc">
+        <h2>Connect with Us</h2>
+        <p id="plea">
+          Have a problem you want to share with us ? do go through the FAQ page
+          to see if there is solution already. Still want to talk to us ? fill
+          out the form and report your big problem with us we respond within
+          10mins of delivery.
+        </p>
+        <img src="../assets/images/contact-illustration.svg" alt="" srcset="" />
+        <p class="call">Kindly call us at:</p>
+        <a href="tel:+2349015379412" id="call_link">+234 9015379412</a>
+        <p class="mail">Email us at:</p>
+        <a href="mailto:" id="email_link">info@ezexchange.com</a>
+      </div>
+      <div class="form-holder">
+        <form action="#" method="post">
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="please enter your email address"
+            />
+          </div>
+          <div class="form-group">
+            <label for="subject">Subject:</label>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="eg, Security, failed transaction, inability to continue etc"
+            />
+          </div>
+          <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <input type="submit" value="Send Message" />
+        </form>
+      </div>
+    </div>
+  </div>
+  <footer-component />
+</template>
 
+<script>
+import navComponent from "../components/Nav-component.vue";
+import footerComponent from "../components/Footer-component.vue";
+export default {
+  name: "aboutView",
+  components: {
+    navComponent,
+    footerComponent,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  background: linear-gradient(170deg, #0f64c1 1.56%, #152a41 140%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+}
+#about-hero {
+  background-color: #eef2f5;
+  .about-hero {
+    padding: 1rem 4rem 1.5rem 4rem;
+    // width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 70%;
+      height: auto;
+    }
+    .text-holder {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-content: center;
+      text-align: left;
+    }
+  }
+}
+#operation {
+  .operation {
+    margin: 0px 4rem;
+    .header {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      div {
+        width: 85%;
+        height: 0px;
+        border: 1px solid #57595aab;
+      }
+    }
+    .content {
+      display: grid;
+      grid-template-columns: 65% 1fr;
+      justify-items: center;
+      width: 100%;
+      .text-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        text-align: left;
+        width: 90%;
+        margin: 0px auto;
+        h4 {
+          align-self: flex-start;
+        }
+        p {
+          width: 100%;
+        }
+      }
+      img {
+        width: 80%;
+        height: 80%;
+      }
+    }
+  }
+  .beginnings {
+    display: grid;
+    grid-template-columns: 35% 1fr;
+    justify-items: end;
+    align-items: center;
+    img {
+      width: 80%;
+      height: 80%;
+    }
+    .text-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      text-align: left;
+      width: 90%;
+      margin: 0px auto;
+      h3,
+      h4 {
+        align-self: flex-start;
+        margin: 8px 0px;
+      }
+      p {
+        width: 100%;
+      }
+    }
+  }
+}
+#about-founder {
+  background-image: url(../assets/images/about_femi_background.svg);
+  background-color: #eef2f5;
+  .about-founder {
+    margin: 0px 4rem;
+    .title {
+      text-align: left;
+    }
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      justify-content: center;
+      align-items: start;
+      img {
+        width: 80%;
+        height: 100%;
+      }
+      .text-content {
+        display: flex;
+        align-items: flex-start;
+        flex-direction: column;
+        text-align: left;
+        width: 90%;
+        margin: 0px auto;
+        color: #ffffff;
+        .big {
+          font-size: 24px;
+        }
+        #femi-about {
+          font-size: 14px;
+        }
+      }
+    }
+  }
+}
+#contact-us {
+  background: #eef2f5;
+
+  border-top: 1px solid #062f5a10;
+  padding: 1rem 0px;
+  .contact {
+    margin: 0px 4rem;
+    display: grid;
+    grid-template-columns: 40% 60%;
+    .desc {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 1rem 0px;
+      h2,
+      p {
+        margin: 8px 0px;
+        text-align: left;
+      }
+      a {
+        margin: 2px 0px;
+        text-decoration: none;
+        color: #152a41;
+        font-size: 14px;
+        font-weight: 600;
+      }
+      img {
+        align-self: center;
+      }
+    }
+    .form-holder {
+      padding: 1rem 0px;
+      width: 100%;
+      text-align: left;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      form {
+        padding: 8px;
+        border-radius: 8px;
+        width: 80%;
+        height: 80%;
+        margin: auto;
+        .form-group {
+          margin: 16px 0px;
+        }
+        input[type="email"],
+        input[type="text"],
+        textarea {
+          width: 100%;
+          max-width: 100%;
+          outline: none;
+          border: none;
+          margin: 4px 0px;
+          border-radius: 5px;
+          padding: 16px 0px 16px 8px;
+        }
+        input[type="email"],
+        input[type="text"] {
+          height: fit-content;
+        }
+        input[type="submit"] {
+          width: fit-content;
+          height: fit-content;
+          padding: 10px 16px;
+          background-color: #0f64c1;
+          color: #fff;
+          border-radius: 5px;
+          margin: 8px 0px;
+          border: none;
+          outline: none;
+          font-weight: 500;
+        }
+      }
+    }
+  }
+}
+</style>
